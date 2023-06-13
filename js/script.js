a0e6b7257ded34c2c27ba2c28d575e175fe71128
modjordan <mod.jordan@icloud.com>
2023-06-13 15:59:45 -0700

    practice.

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+function getAndSetMadLibValues() {
+  const person1Input = document.getElementById("person1Input").value;
+  const person2Input = document.getElementById("person2Input").value;
+  const animalInput = document.getElementById("animalInput").value;
+  const exclamationInput = document.getElementById("exclamationInput").value;
+  const verbInput = document.getElementById("verbInput").value;
+  const nounInput = document.getElementById("nounInput").value;
+  document.querySelector("span#person1a").innerText = person1Input;
+  document.querySelector("span#person1b").innerText = person1Input;
+  document.querySelector("span#person1c").innerText = person1Input;
+  document.querySelector("span#person2a").innerText = person2Input;
+  document.querySelector("span#person2b").innerText = person2Input;
+  document.querySelector("span#animal").innerText = animalInput;
+  document.querySelector("span#verb").innerText = verbInput;
+  document.querySelector("span#noun").innerText = nounInput;
+  document.querySelector("span#exclamation").innerText = exclamationInput;
+  document.querySelector("div#story").removeAttribute("class");
+}
+
+
+
+window.onload = function () {
+  let form = document.querySelector("form");
+  form.onsubmit = function (e) {
+    e.preventDefault();
+    getAndSetMadLibValues();
+    document.querySelector("div#story").removeAttribute("class");
+  };
+};
